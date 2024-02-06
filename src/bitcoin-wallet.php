16142b4371e866205c38c140a925a0b5e4ef361d
isaack-njama <41724425+isaack-njama@users.noreply.github.com>
2024-02-06 11:21:50 +0000

    feat: create and sign transaction

--- src/bitcoin-wallet.php
+++ src/bitcoin-wallet.php
@@ -9,28 +9,24 @@ use BitWasp\Bitcoin\Address\SegwitAddress;
 use BitWasp\Bitcoin\Bitcoin;
 use BitWasp\Bitcoin\Crypto\Random\Random;
 use BitWasp\Bitcoin\Key\Factory\HierarchicalKeyFactory;
-use BitWasp\Bitcoin\Key\Factory\PrivateKeyFactory;
 use BitWasp\Bitcoin\Mnemonic\Bip39\Bip39Mnemonic;
 use BitWasp\Bitcoin\Mnemonic\Bip39\Bip39SeedGenerator;
 use BitWasp\Bitcoin\Mnemonic\MnemonicFactory;
 use BitWasp\Bitcoin\Network\NetworkFactory;
-use BitWasp\Bitcoin\Script\WitnessProgram;
 use BitWasp\Bitcoin\Script\ScriptFactory;
-use BitWasp\Bitcoin\Script\Opcodes;
-use BitWasp\Bitcoin\Script\ScriptType;
-use BitWasp\Bitcoin\Script\P2shScript;
+use BitWasp\Bitcoin\Script\WitnessProgram;
 use BitWasp\Bitcoin\Transaction\Factory\TxBuilder;
 use BitWasp\Bitcoin\Transaction\Factory\Signer;
+use BitWasp\Bitcoin\Transaction\Factory\SignData;
 use BitWasp\Buffertools\Buffer;
 use BitWasp\Bitcoin\Transaction\OutPoint;
+use BitWasp\Bitcoin\Transaction\TransactionOutput;
+use BitWasp\Bitcoin\Crypto\EcAdapter\Impl\PhpEcc\Key\PrivateKey;
 
 Bitcoin::setNetwork(NetworkFactory::bitcoinTestnet());
 $network = Bitcoin::getNetwork();
 $ecAdapter = Bitcoin::getEcAdapter();
-
-function bytes_to_hex($byte_str) {
-    return bin2hex($byte_str);
-}
+$addrCreator = new AddressCreator();
 
 // Generate a mnemonic
 $random = new Random();
@@ -57,12 +53,16 @@ $accountKey = $purposeKey->derivePath('0');
 // Derive the external key (m/84'/0'/0'/0/0)
 $externalKey = $accountKey->derivePath('0');
 
+// Generate a private key from the external key
+$privateKey = $externalKey->getPrivateKey();
+echo "Private Key: " . $privateKey->getHex() . "\n";
+
 // Get the public key from the external key
 $publicKey = $externalKey->getPublicKey();
 
 // Derive the public key hash
 $publicKeyHash = $publicKey->getPubKeyHash();
-echo 'Public Key Hash: ' . $publicKeyHash->getHex() . "\n";
+echo "Public Key Hash: " . $publicKeyHash->getHex() . "\n";
 
 // Derive the pay to public key hash address
 $p2pkh = new PayToPubKeyHashAddress($publicKeyHash);
@@ -90,30 +90,35 @@ $p2wpkhWP = WitnessProgram::v0($publicKeyHash);
 $p2wpkh = new SegwitAddress($p2wpkhWP);
 $p2wpkhaddress = $p2wpkh->getAddress($network);
 echo 'Native SegWit / P2WPKH Address: ' . $p2wpkhaddress . "\n";
+echo "Private Key: " . $privateKey->getHex() . "\n";
 
 // Derive the Pay to Witness Script Hash address
 $p2wshWP = WitnessProgram::v0($redeemScript->getWitnessScriptHash());
 $p2wsh = new SegwitAddress($p2wshWP);
 $p2wshAddress = $p2wsh->getAddress($network);
-echo 'P2WSH Address: ' . $p2wshAddress . "\n";
+echo 'P2WSH Address: ' . $p2wshAddress . "\n\n";
 
-// Address used: tb1q6qrae368rg5jpze6huc76qg37ecmucmhpjqa9t (Native Segwit)
 // Construct a transaction
-$addrCreator = new AddressCreator();
 $txID = 'e31d31c51f01f8e99d9f4c7ddac6b5554c7d119907c3807e388a1f2c793fdcb0';
-$spendOutput = new Outpoint(Buffer::hex('e31d31c51f01f8e99d9f4c7ddac6b5554c', 32), 0);
-$recipient = $p2shAddress;
+$spendOutput = new OutPoint(Buffer::hex('e31d31c51f01f8e99d9f4c7ddac6b5554c', 32), 0);
+$recipientScript = $p2wpkh->getScriptPubKey();
 
-// Construct a transaction
+$txOut = new TransactionOutput(99990000, $recipientScript);
+$recipient = $addrCreator->fromString($p2wpkhaddress);
+
 $tx = (new TxBuilder())
     ->spendOutPoint($spendOutput)
-    ->payToAddress(10000, $p2pkh)
+    ->payToAddress(10000, $recipient)
     ->get();
 
+$signData = (new SignData())
+    ->p2wsh;
+
 // Sign the transaction
-$signer = new Signer($tx, $ecAdapter);
-$signedTx = $signer->sign(0, $externalKey, $txID);
+$signer = new Signer($tx);
+$input = $signer->input(0, $txOut, $signData);
+$input -> sign($privateKey);
+$signed = $signer->get();
 
-echo $signedTx->getHex() . "\n";
-
+echo $signed->getHex() . "\n";
 ?>
